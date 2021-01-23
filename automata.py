754acdc5ff5e439c04fac0975c8aa2d9499b6cf7
Lachlan Gray <lachlangray@Lachlans-MacBook-Pro.local>
2021-01-23 14:12:03 -0500

    added set default state

--- automata.py
+++ automata.py
@@ -25,6 +25,10 @@ class Automata():
 	def update_rule(self, new_rule):
 		self.rule = {local_state:action for local_state, action in zip(self.rule.keys(), new_rule)}
 
+	def set_default_state(self, state):
+		self.default_state = np.concatenate((state[-self.l:], state, state[:self.l]))
+		self.N = len(state)
+
 
 	def tick(self):
 		state = np.array([self.rule[self.state[a:b].tobytes()] for a, b in zip(range(0,self.N), range(2*self.r + 1, self.N + 2*self.r + 1))])
